41a40c5bb3305717e98a8436a8217fcdbc1ba4eb
root <root@dustbuilder.xvm.mit.edu>
2020-12-31 17:41:51 -0500

    adding dustbuilder

---
+++ fwinstaller/install_b.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Author: Dennis Giese [dgiese at dontvacuum.me]
+# Copyright 2020 by Dennis Giese
+# 
+# Intended to work on v1,s4,s5,s6,t4,t6
+#
+# This tool installs the partition image on System_B and patches the root password
+# System_A is marked then as BAD in order to force the next boot from System_B
+# It needs to be executed with System_A being active
+
+echo "---------------------------------------------------------------------------"
+echo " Xiaomi/Roborock/Rockrobo manual Firmware installer Stage 1"
+echo " Copyright 2020 by Dennis Giese [dgiese at dontvacuum.me]"
+echo " Intended to work on v1, s4, s5, s6, t4, t6"
+echo " Use at your own risk"
+echo "---------------------------------------------------------------------------"
+
+
+grep -q "boot_fs=a" /proc/cmdline
+if [ $? -eq 1 ]
+then
+   echo "(!!!) You did not boot into System_A. It is not safe to execute this script"
+   exit 1
+fi
+
+md5sum -c firmware.md5sum
+if [ $? -eq 0 ]; then
+	echo "(!!!) integrity check failed. Firmware files are damaged. Please re-download the firmware. Aborting the installation"
+	exit 1
+fi 
+
+
+if [[ -f /sbin/cleanflags.sh ]]; then
+   echo "detected resetfix, disabling it"
+   echo "#!/bin/bash" > /sbin/cleanflags.sh
+   echo "exit 0" >> /sbin/cleanflags.sh 	
+fi
+
+if [[ -f /mnt/data/disk.img ]]; then
+	echo "Installing(this make take a few minutes) ..."
+	dd if=/mnt/data/disk.img of=/dev/mmcblk0p9
+	echo "marking System_A as BAD (this is expected)"
+	echo -n -e '\x4' | dd conv=notrunc of="/dev/mmcblk0p5" bs=1 count=1 seek=309504
+	mkdir /mnt/a
+	mount /dev/mmcblk0p9 /mnt/a
+	cp /etc/shadow /mnt/a/etc/shadow
+	umount /mnt/a
+	sync
+	echo "---------------------------------------------------------------------------"
+	echo "Please reboot the robot, login again and execute install_a.sh in /mnt/data"
+	echo "---------------------------------------------------------------------------"
+else
+	echo "(!!!) disk.img not found in /mnt/data"
+fi
---
+++ fwinstaller_nand/install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Author: Dennis Giese [dgiese at dontvacuum.me]
+# Copyright 2020 by Dennis Giese
+# 
+# Intended to work on s5e,p5,a08,a11
+#
+echo "---------------------------------------------------------------------------"
+echo " Roborock manual Firmware installer"
+echo " Copyright 2020 by Dennis Giese [dgiese at dontvacuum.me]"
+echo " Intended to work on s5e, p5, a08, a11"
+echo " Use at your own risk"
+echo "---------------------------------------------------------------------------"
+
+grep -q "boot_fs=a" /proc/cmdline
+if [ $? -eq 1 ]
+then
+   echo "(!!!) You are booted currently in the backup copy of your operation system. This installer does not supports this case!"
+   exit 1
+fi
+
+grep -q "nande" /proc/cmdline
+if [ $? -eq 1 ]
+then
+   echo "(!!!) It seems you are trying to run the installer on the wrong device."
+   exit 1
+fi
+
+echo "preparing installation"
+  cp /bin/busybox /tmp/busybox
+  chmod +x /tmp/busybox
+  ln -s /tmp/busybox /tmp/reboot
+  ln -s /tmp/busybox /tmp/bash
+
+echo "check image file size"
+  maximumsize=26000000
+  minimumsize=20000000
+  actualsize=$(wc -c < /mnt/data/rootfs.img)
+  if [ $actualsize -ge $maximumsize ]; then
+    echo "(!!!) rootfs.img looks to big. The size might exceed the available space on the flash. Aborting the installation"
+    exit 1
+  fi
+  if [ $actualsize -le $minimumsize ]; then
+    echo "(!!!) rootfs.img looks to small. Maybe something went wrong with the image generation. Aborting the installation"
+    exit 1
+  fi
+
+if [[ -f /mnt/data/boot.img ]]; then
+  if [[ -f /mnt/data/rootfs.img ]]; then
+	echo "Checking integrity"
+	md5sum -c firmware.md5sum
+	if [ $? -eq 0 ]; then
+		echo "(!!!) integrity check failed. Firmware files are damaged. Please re-download the firmware. Aborting the installation"
+		exit 1
+	fi 
+	echo "Start installation ..."
+	echo "Installing Kernel on kernel_a"
+	dd if=/mnt/data/boot.img of=/dev/nandc bs=8192
+	echo "Installing Kernel on kernel_b"
+	dd if=/mnt/data/boot.img of=/dev/nandd bs=8192
+	rm /mnt/data/boot.img
+	echo "Installing OS in system_b"
+	dd if=/mnt/data/rootfs.img of=/dev/nandf bs=8192
+	echo "Trying to mount system_b"
+	mkdir /tmp/system_b
+	mount /dev/nandf /tmp/system_b
+	if [ ! -f /tmp/system_b/build.txt ]; then
+	    echo "(!!!) Did not found marker in updated firmware. Update likely failed, wont update system_a."
+	    exit 1
+	fi
+	rm /mnt/data/rootfs.img
+	rm /mnt/data/firmware.md5sum
+	echo "Installing OS in system_a"
+	/sbin/reboot -h >/dev/null 2>&1
+	/tmp/reboot -h >/dev/null 2>&1
+	dd if=/dev/nandf of=/dev/nande bs=8192
+	/tmp/reboot
+
+	echo "----------------------------------------------------------------------------------"
+	echo "Done, rebooting"
+	echo "Dont forget to delete the installer files after rebooting"
+	echo "----------------------------------------------------------------------------------"
+  else
+	echo "(!!!) rootfs.img not found in /mnt/data"
+  fi
+else
+	echo "(!!!) boot.img not found in /mnt/data"
+fi
