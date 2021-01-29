c5daa8f9dc2ff6e35e0c68b5ed4b58c119e24640
dgiese <dgiese@mit.edu>
2021-01-29 06:10:12 -0500

    new ip method

--- valetudo/deployment/valetudo-daemon.sh
+++ valetudo/deployment/valetudo-daemon.sh
@@ -17,7 +17,7 @@ while :; do
     if [ $? -ne 0 ]; then
         echo "Running Valetudo"
         echo '|/bin/false' > /proc/sys/kernel/core_pattern
-		### ifconfig lo:1 203.0.113.1
+		### ip addr add 203.0.113.1 dev lo
         if [ -f "/root/bin/busybox" ]; then
             (
                 # Make valetudo very likely to get killed when out of memory
