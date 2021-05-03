913dd616ba2c141a9f5cd31f585e735fd45e0831
dgiese <dgiese@ccs.neu.edu>
2021-05-03 01:34:54 -0400

    sanitize stuff

---
+++ fwinstaller_1t/sanitize.sh
@@ -0,0 +1,8 @@
+rm etc/OTA_Key_pub.pem
+rm etc/adb_keys
+rm etc/publickey.pem
+rm usr/bin/autossh.sh
+rm usr/bin/backup_key.sh
+rm usr/bin/curl_download.sh
+rm usr/bin/curl_upload.sh
+rm usr/bin/packlog.sh
