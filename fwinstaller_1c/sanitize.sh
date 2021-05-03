a59acbfff0e7408171a3117ec7c976d4d8e79382
dgiese <dgiese@ccs.neu.edu>
2021-05-03 02:43:42 -0400

    x

--- fwinstaller_1c/sanitize.sh
+++ fwinstaller_1c/sanitize.sh
@@ -1,8 +1,8 @@
-rm etc/OTA_Key_pub.pem
-rm etc/adb_keys
-rm etc/publickey.pem
-rm usr/bin/autossh.sh
-rm usr/bin/backup_key.sh
-rm usr/bin/curl_download.sh
-rm usr/bin/curl_upload.sh
-rm usr/bin/packlog.sh
+rm ./squashfs-root/etc/OTA_Key_pub.pem
+rm ./squashfs-root/etc/adb_keys
+rm ./squashfs-root/etc/publickey.pem
+rm ./squashfs-root/usr/bin/autossh.sh
+rm ./squashfs-root/usr/bin/backup_key.sh
+rm ./squashfs-root/usr/bin/curl_download.sh
+rm ./squashfs-root/usr/bin/curl_upload.sh
+rm ./squashfs-root/usr/bin/packlog.sh
