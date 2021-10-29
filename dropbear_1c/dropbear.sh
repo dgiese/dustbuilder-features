198fb71218a4bf4dcc4593bb1a8718c5c9bbeec0
dgiese <dgiese@ccs.neu.edu>
2021-10-29 00:08:28 -0400

    dropbear scripts

--- dropbear_1c/dropbear.sh
+++ dropbear_1c/dropbear.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 source /usr/bin/config
 mkdir /tmp/.ssh
+
 if [  ! -f /mnt/misc/authorized_keys ]; then
-        cp /authorized_keys /mnt/misc/authorized_keys
+    cp /authorized_keys /mnt/misc/authorized_keys
 fi
 cp /mnt/misc/authorized_keys /tmp/.ssh/
-dropbear&
+
+# check if password login for ssh should be disabled
+if [ -f /mnt/misc/ssh_disable_passwords ]; then
+    dropbear -s &
+else
+    dropbear &
+fi
+
